dbe2e371da068b74f551747368a770b1200a44b5
Gerdin Ventura Croussett <gerdinventuraedu@gmail.com>
2021-12-07 02:12:41 -0500

    inital commit

---
+++ Flix/FlixApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlixApp.swift
+//  Flix
+//
+//  Created by Gerdin Ventura on 12/7/21.
+//
+
+import SwiftUI
+
+@main
+struct FlixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
